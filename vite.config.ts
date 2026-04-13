7eca526bf57a3c9f87b2e6e6ca814722e8ed1481
Ana Carolina <anaaraujo.dev@gmail.com>
2026-04-13 14:14:21 -0300

    fix: update basepath

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,20 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
+    server: {
+        host: "::",
+        port: 8080,
+        hmr: {
+            overlay: false,
+        },
     },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+        Boolean,
+    ),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  base: "/portfolio/",
+    base: "/",
 }));
